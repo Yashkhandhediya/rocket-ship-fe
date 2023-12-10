a9d21b8374b02be3bc2314e32bb743523e033972
suraj-kumbhani <surajkumbhani925@gmail.com>
2023-12-10 15:57:49 +0530

    fix: tabs component

--- src/common/components/tabs/Tabs.js
+++ src/common/components/tabs/Tabs.js
@@ -1,24 +1,25 @@
+import { useState } from 'react';
 import { infoIcon } from '../../icons';
 
 const Tabs = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(0);
   return (
     <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
       <ul
         className="-mb-px flex flex-wrap text-center text-sm font-medium"
-        id="default-tab"
-        data-tabs-toggle="#default-tab-content"
-        role="tablist">
+        id="default-tab">
         {tabs.map((tab, i) => {
           return (
-            <li className="me-2 flex" role="presentation" key={tab.id}>
+            <li className="me-2 flex" key={tab.id}>
               <button
-                className="inline-flex items-center rounded-t-lg border-b-4 p-2 font-normal"
+                className={`inline-flex items-center rounded-t-lg border-b-4 p-2 font-normal ${
+                  i === activeTab
+                    ? 'border-indigo-600 text-indigo-600'
+                    : 'border-transparent'
+                }`}
                 id={`${tab.id}-tab`}
-                data-tabs-target={`#${tab.id}-panel`}
                 type="button"
-                role="tab"
-                aria-controls={`${tab.id}-panel`}
-                aria-selected={i === 0 ? 'true' : 'false'}>
+                onClick={() => setActiveTab(i)}>
                 {tab.title}
                 <img className="ms-2" src={infoIcon} />
               </button>
@@ -27,14 +28,14 @@ const Tabs = ({ tabs }) => {
         })}
       </ul>
       <div id="default-tab-content">
-        {tabs.map((tab) => {
+        {tabs.map((tab, i) => {
           return (
             <div
               key={tab.id}
-              className={`hidden rounded-lg bg-gray-50  dark:bg-gray-800`}
-              id={`${tab.id}-panel`}
-              role="tabpanel"
-              aria-labelledby={`${tab.id}-tab`}>
+              className={`rounded-lg bg-gray-50  dark:bg-gray-800 ${
+                i !== activeTab ? 'hidden' : ''
+              }`}
+              id={`${tab.id}-panel`}>
               {tab.panel}
             </div>
           );
