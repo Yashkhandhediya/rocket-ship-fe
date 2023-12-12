aec9dcb915c0211933e9529dfd752258c3339daa
suraj-kumbhani <surajkumbhani925@gmail.com>
2023-12-12 23:16:21 +0530

    feat: pickup details page integrated

---
+++ src/common/address-verified-tag/AddressVerifiedTag.js
@@ -0,0 +1,15 @@
+import { varifiedTickmark } from '../icons';
+
+const AddressVerifiedTag = () => {
+  return (
+    <>
+      <span className="absolute right-0 top-4 z-50 flex text-[8px] font-bold text-white ">
+        <img src={varifiedTickmark} className="pr-0.5" />
+        {'Verified'}
+      </span>
+      <div className="absolute  right-[-0.6rem] top-4 w-16 border-l-[15px] border-t-[15px] border-green-600 border-l-transparent after:absolute after:right-0 after:top-0 after:border-l-0 after:border-r-[9px] after:border-t-[5px] after:border-l-transparent after:border-r-transparent after:border-t-green-600"></div>
+    </>
+  );
+};
+
+export default AddressVerifiedTag;
