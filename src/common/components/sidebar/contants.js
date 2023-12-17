151c265b00ed4f9e6d95911d0cfdcd3aeb0ab932
suraj-kumbhani <surajkumbhani925@gmail.com>
2023-12-17 10:39:38 +0530

    feat: sidebar animation

---
+++ src/common/components/sidebar/contants.js
@@ -0,0 +1,144 @@
+import { home, homeActive } from "../../icons/sidebar-icons";
+
+export const sidebarLinks = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+  {
+    title: 'Dashboard',
+    path: '/dashboard',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+  {
+    title: 'Orders',
+    path: '/orders',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+  {
+    title: 'Returns',
+    path: '/',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+  {
+    title: 'Delivery Boost',
+    path: '/',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+  {
+    title: 'Weight Management',
+    icon: home,
+    hoverIcon: homeActive,
+    subMenuOptions: [
+      {
+        title: 'Weight Discrepancy',
+        path: '/',
+      },
+      {
+        title: 'Weight Freeze',
+        path: '/',
+      },
+    ],
+  },
+  {
+    title: 'Setup & Manage',
+    icon: home,
+    hoverIcon: homeActive,
+    subMenuOptions: [
+      {
+        title: 'Channels',
+        path: '/',
+      },
+      {
+        title: 'Catalogue',
+        path: '/',
+      },
+      {
+        title: 'Courier',
+        path: '/',
+      },
+      {
+        title: 'Packaging',
+        path: '/',
+      },
+      {
+        title: 'Customers',
+        path: '/',
+      },
+    ],
+  },
+  {
+    title: 'Buyer Experiance',
+    icon: home,
+    hoverIcon: homeActive,
+    subMenuOptions: [
+      {
+        title: 'Brand Boost',
+        path: '/',
+      },
+      {
+        title: 'Brand Communication',
+        path: '/',
+      },
+      {
+        title: 'Return & Refunds',
+        path: '/',
+      },
+    ],
+  },
+  {
+    title: 'Billing',
+    path: '/',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+  {
+    title: 'Tools',
+    icon: home,
+    hoverIcon: homeActive,
+    subMenuOptions: [
+      {
+        title: 'Rate Calculator',
+        path: '/',
+      },
+      {
+        title: 'Rate Card',
+        path: '/',
+      },
+      {
+        title: 'Pincode Zone Mapping',
+        path: '/',
+      },
+      {
+        title: 'Bulk Print Actions',
+        path: '/',
+      },
+      {
+        title: 'Reports',
+        path: '/',
+      },
+      {
+        title: 'Activity Logs',
+        path: '/',
+      },
+    ],
+  },
+  {
+    title: 'Settings',
+    path: '/',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+  {
+    title: 'Help & Support',
+    path: '/',
+    icon: home,
+    hoverIcon: homeActive,
+  },
+];
