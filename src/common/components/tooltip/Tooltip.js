283c57775aa10a8f5209a784b1e8e6fc0fbebc6d
suraj-kumbhani <surajkumbhani925@gmail.com>
2023-12-24 16:46:33 +0530

    feat: copy to clipboard order id

--- src/common/components/tooltip/Tooltip.js
+++ src/common/components/tooltip/Tooltip.js
@@ -1,12 +1,13 @@
 import { Tooltip as FlowbiteTooltip } from 'flowbite-react';
 
-const Tooltip = ({ children, text,wrapperClassNames, ...rest }) => {
+const Tooltip = ({ children, text, wrapperClassNames, width, ...rest }) => {
   return (
     <FlowbiteTooltip
       style="light"
       content={text}
-      className={`inline-flex min-w-[150px] max-w-[250px] text-xs font-normal text-gray-700 shadow-xl ${wrapperClassNames}`}
-
+      className={`inline-flex ${
+        width ? `min-w-[${width}]` : 'min-w-[150px]'
+      } max-w-[250px] text-xs font-normal text-gray-700 shadow-xl ${wrapperClassNames}`}
       {...rest}>
       {children}
     </FlowbiteTooltip>
--- src/pages/order-track-details/OrderTrackDetails.js
+++ src/pages/order-track-details/OrderTrackDetails.js
@@ -1,8 +1,8 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import PageWithSidebar from '../../common/components/page-with-sidebar/PageWithSidebar';
 import { blackLeftArrow, copyToClipboard, moreAction } from '../../common/icons';
 import { Badge } from 'flowbite-react';
-import { MoreDropdown } from '../../common/components';
+import { MoreDropdown, Tooltip } from '../../common/components';
 import { moreActionOptions } from '../orders/components/new/utils';
 import OrderDetailsCard from './components/OrderDetailsCard';
 import PackageDetailsCard from './components/PackageDetailsCard';
@@ -10,20 +10,44 @@ import CustomerDetailsCard from './components/CustomerDetailsCard';
 import ProductDetailsCard from './components/ProductDetailsCard';
 import AppChangesCard from './components/AppChangesCard';
 import OrderTrackRightContainer from './components/OrderTrackRightContainer';
+import { useState } from 'react';
 
 const OrderTrackDetails = () => {
   const { orderId } = useParams();
+  const [copyTooltip, setCopyTooltip] = useState('Click to Copy');
+
+  const copyOrderId = () => {
+    navigator.clipboard.writeText(orderId);
+    setCopyTooltip('Copied!');
+    setTimeout(() => {
+      setCopyTooltip('Click to Copy');
+    }, 1000);
+  };
 
   return (
     <PageWithSidebar>
-      <div className="w-full bg-[#f8f8f8] pt-9 pb-5">
+      <div className="w-full bg-[#f8f8f8] pb-5 pt-9">
         <div className="mx-auto h-full max-w-[1180px]">
           <div className="w-full">
             <div className="flex items-center justify-between px-2 md:w-8/12">
               <div className="flex items-center">
-                <img src={blackLeftArrow} className="mr-0.5 h-4 w-4 cursor-pointer" />
+                <Link to="/orders">
+                  <img src={blackLeftArrow} className="mr-0.5 h-4 w-4 cursor-pointer" />
+                </Link>
                 <div className="flex items-center text-lg font-medium">
-                  #{orderId} <img src={copyToClipboard} className="ml-2 h-3 w-3" />
+                  #{orderId}
+                  <Tooltip
+                    style="dark"
+                    text={copyTooltip}
+                    arrow={false}
+                    wrapperClassNames={'text-gray-200 bg-gray-600'}
+                    width="auto">
+                    <img
+                      src={copyToClipboard}
+                      className="ml-2 h-3 w-3 cursor-pointer"
+                      onClick={copyOrderId}
+                    />
+                  </Tooltip>
                 </div>
                 <Badge
                   color="success"
@@ -41,14 +65,13 @@ const OrderTrackDetails = () => {
                 />
               </div>
             </div>
-            <div className="w-full flex">
+            <div className="flex w-full">
               <div className="px-2 md:w-8/12">
                 <OrderDetailsCard orderDetails={{}} />
                 <PackageDetailsCard packageDetails={{}} />
                 <CustomerDetailsCard customerDetails={{}} />
-                <ProductDetailsCard productDetails={[1,2]} />
+                <ProductDetailsCard productDetails={[1, 2]} />
                 <AppChangesCard />
-
               </div>
               <div className="px-2 md:w-4/12">
                 <OrderTrackRightContainer />
