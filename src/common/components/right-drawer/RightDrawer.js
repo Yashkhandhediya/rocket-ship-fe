4aa282ee44d13679636c24bbaa8c2b183203af2a
suraj-kumbhani <surajkumbhani925@gmail.com>
2023-12-27 10:34:20 +0530

    feat: shipment buyer more details drawer

--- src/common/components/right-drawer/RightDrawer.js
+++ src/common/components/right-drawer/RightDrawer.js
@@ -1,30 +1,26 @@
-const RightDrawer = ({ isOpen, heading, onClose, children }) => {
+const RightDrawer = ({ isOpen, heading, onClose, children, drawerWrapperClassNames }) => {
   return (
     <>
       <div
-        className={`dark:bg-gray-800 fixed right-0 top-0 z-40 h-screen w-[80%] max-w-[80vw] overflow-y-auto rounded-lg bg-white px-5 py-4 transition-transform ${
+        className={`dark:bg-gray-800 fixed right-0 top-0 z-40 h-screen w-[80%] max-w-[80vw] rounded-lg bg-white px-5 py-4 transition-transform ${
           isOpen ? 'transform-none' : 'translate-x-full'
-        }`}
-        tabIndex="-1"
-      >
+        } ${drawerWrapperClassNames || ''}`}
+        tabIndex="-1">
         <h5
           id="drawer-right-label"
-          className="dark:text-gray-400 inline-flex items-center text-lg font-semibold text-gray-900"
-        >
+          className="dark:text-gray-400 inline-flex items-center text-lg font-semibold text-gray-900">
           {heading}
         </h5>
         <button
           type="button"
           className="dark:hover:bg-gray-600 dark:hover:text-white absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
-          onClick={onClose}
-        >
+          onClick={onClose}>
           <svg
             className="h-3 w-3"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            viewBox="0 0 14 14"
-          >
+            viewBox="0 0 14 14">
             <path
               stroke="currentColor"
               strokeLinecap="round"
@@ -37,7 +33,7 @@ const RightDrawer = ({ isOpen, heading, onClose, children }) => {
         </button>
         <div className="mb-4 mt-4 w-full border border-gray-100" />
 
-        <div>{children}</div>
+        <div className="w-full h-full overflow-auto pb-5" style={{scrollbarGutter: "stable"}}>{children}</div>
       </div>
       {isOpen && (
         <div className="dark:bg-gray-900/80 fixed inset-0 z-30 bg-gray-900/50" onClick={onClose}></div>
--- src/pages/orders/components/shipment-drawer-order-details/ShipmentDrawerOrderDetails.js
+++ src/pages/orders/components/shipment-drawer-order-details/ShipmentDrawerOrderDetails.js
@@ -1,7 +1,11 @@
+import { Badge } from 'flowbite-react';
+import RightDrawer from '../../../../common/components/right-drawer/RightDrawer';
 import { openBox } from '../../../../common/icons';
 import LabelValue from './components/LabelValue';
+import { useState } from 'react';
 
 const ShipmentDrawerOrderDetails = ({ orderDetails }) => {
+  const [openMoreDetails, setOpenMoreDetails] = useState(false);
   return (
     <div>
       <LabelValue
@@ -43,11 +47,69 @@ const ShipmentDrawerOrderDetails = ({ orderDetails }) => {
             <img src={openBox} className="inline-flex w-[1.875rem]" />
             <span className="mb-3 ml-2.5 inline-flex">{'No Orders'}</span>
           </div>
-          <button className="text-wrap rounded-md border border-indigo-700 bg-transparent px-6 py-2 text-xs text-indigo-700">
+          <button
+            className="text-wrap rounded-md border border-indigo-700 bg-transparent px-6 py-2 text-xs text-indigo-700"
+            onClick={() => setOpenMoreDetails(true)}>
             {'View All Details'}
           </button>
         </div>
       </div>
+      <RightDrawer
+        isOpen={openMoreDetails}
+        onClose={() => setOpenMoreDetails(false)}
+        heading={'Buyer Insights'}
+        drawerWrapperClassNames={'max-w-[375px] px-0'}>
+        <div className="w-full">
+          <LabelValue label={'Buyer Name'} value={orderDetails?.buyer_info?.first_name} />
+          <div className="mb-2 mt-4 w-full border border-gray-100" />
+          <div className="mb-6 inline-flex items-center text-lg font-semibold text-gray-900">
+            {'Buyer History On Your Store'}
+          </div>
+          <div>
+            <div className="mb-2.5 text-xs font-medium">{'Orders in Last 90 Days'}</div>
+            <div className="mb-2.5 text-xs font-medium">{'0'}</div>
+          </div>
+        </div>
+        <div>
+          <div className="mb-2.5 text-xs font-medium">{'Last Order Details'}</div>
+          <div className="flex justify-between">
+            <LabelValue label={'Purchase Date'} value={orderDetails?.date} />
+            <LabelValue
+              label={'Status'}
+              value={
+                <Badge color={'green'} className="border border-green-600 py-0 text-xs">
+                  {'PENDING'}
+                </Badge>
+              }
+            />
+          </div>
+        </div>
+        <div className="mb-2 mt-4 w-full border border-gray-100" />
+        <div>
+          <div className="mb-5 inline-flex items-center text-lg font-semibold text-gray-900">
+            {'Last Successful Delivery To Buyer:'}
+          </div>
+          <div className="text-wrap mb-3 text-xs font-medium">{'On Your Store'}</div>
+          <div className="text-wrap mb-3 text-xs font-medium">
+            <img src={openBox} className="inline-flex w-[1.875rem]" />
+            <span className="mb-3 ml-2.5 inline-flex">{'No Orders'}</span>
+          </div>
+          <div className="text-wrap mb-3 text-xs font-medium">{'On Ship Rocket'}</div>
+          <div className="text-wrap mb-3 text-xs font-medium">
+            <img src={openBox} className="inline-flex w-[1.875rem]" />
+            <span className="mb-3 ml-2.5 inline-flex">{'No Orders'}</span>
+          </div>
+        </div>
+        <div className="">
+          <div className="mb-2 mt-5 inline-flex items-center text-lg font-bold text-gray-900">
+            {'Was this helpful?'}
+          </div>
+          <div className="flex gap-3">
+            <button className="rounded-md bg-green-600 px-5 py-2 text-sm text-white">{'Yes'}</button>
+            <button className="rounded-md bg-red-600 px-5 py-2 text-sm text-white">{'No'}</button>
+          </div>
+        </div>
+      </RightDrawer>
     </div>
   );
 };
