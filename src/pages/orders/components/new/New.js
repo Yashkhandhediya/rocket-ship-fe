823e5fc529e9cb73ba3ff80ae0d895466e22aa45
nileshpal23 <nilesh.pal2398@gmail.com>
2023-12-28 16:59:12 +0530

    updated new js

--- src/pages/orders/components/new/New.js
+++ src/pages/orders/components/new/New.js
@@ -160,7 +160,7 @@ export const New = () => {
               className="min-w-fit rounded bg-indigo-700 px-4 py-1.5 text-white"
               onClick={() =>{
               const resp = axios.get('http://43.252.197.60:8030/order/track?order_id='+row.id)
-              let newURL =  `http://${window.location.host}/tracking?data=${encodeURIComponent('20')}`
+              let newURL =  `http://${window.location.host}/tracking?data=${encodeURIComponent('15')}`
               console.log('new url', newURL)
               let newTab =window.open(newURL,'_blank');
               if (newTab){
