5a488f0223020072bc994903cb23bf4ce2c77c7c
Yash Khandhediya <yashhkhandhediya@gmail.com>
2024-03-26 00:55:23 +0530

    Added condition for admin in truck booking section for price lock

--- src/pages/indent/Allindent.js
+++ src/pages/indent/Allindent.js
@@ -210,11 +210,12 @@ const Allindent = () => {
                   <div className="flex flex-row justify-between items-end">
                     <div className='mt-4'>
                       <label className='text-xs text-purple-400 font-semibold'>ACTUAL PRICE</label>
-                      <input type="text" value={price[data.id] || ''} onChange={(e) => handlePriceChange(data.id,e.target.value)} className="border w-36 h-10 mt-2 ml-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-100" />
+                      {(data.actual_price == null) ? (<input type="text" value={price[data.id] || ''} onChange={(e) => handlePriceChange(data.id,e.target.value)} className="border w-36 h-10 mt-2 ml-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-100" />) :
+                      (<input type="text" value={`₹${data.actual_price ?? 0}`} disabled onChange={(e) => handlePriceChange(data.id,e.target.value)} className="border w-36 h-10 mt-2 ml-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-100" />)}
                     </div>
                     <div className='mt-4'>
-                      <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg"
-                      onClick={() => {handlePrice(data.id)}}>Save</button>
+                    {(data.actual_price == null) && <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg"
+                      onClick={() => {handlePrice(data.id)}}>Save</button>}
                     </div>
                   </div>
                 ) : (
