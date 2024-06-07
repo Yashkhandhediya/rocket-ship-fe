87f3d7670b72752f6de2b27b99187ae9ad2817ed
Karishma019 <karishmaacharya63@gmail.com>
2024-06-07 18:54:11 +0530

    updated

--- src/pages/sign-up/SingUpUser.js
+++ src/pages/sign-up/SingUpUser.js
@@ -7,17 +7,18 @@ import { toast } from 'react-toastify';
 
 const SignUpUser = () => {
   const navigate = useNavigate();
-  const [flag,setFlag] = useState(0)
+  const [flag, setFlag] = useState(0);
   const [signupInput, setSignupInput] = useState({
     company_id: localStorage.getItem('company_id'),
-    first_name: "",
-    last_name: "",
-    email_address: "",
-    contact_no: "",
-    password: "",
+    first_name: '',
+    last_name: '',
+    email_address: '',
+    contact_no: '',
+    password: '',
     // users_type_id:0
   });
 
+  const [error, setError] = useState({});
 
   const handleChangeInput = (e) => {
     const { id, value } = e.target;
@@ -27,29 +28,52 @@ const SignUpUser = () => {
     });
   };
 
+  const requiredFieldErrors = () => {
+    const newErrors = {};
+
+    if (!signupInput.first_name) newErrors.first_name = 'First Name is Required';
+    if (!signupInput.last_name) newErrors.last_name = 'Last Name is Required';
+    if (!signupInput.email_address) newErrors.email_address = 'email address is Required';
+    if (!signupInput.contact_no) newErrors.contact_no = 'contact no. is Required';
+    if (!signupInput.password) newErrors.password = 'password is Required';
+    return newErrors;
+  };
+
   const handleSubmit = () => {
-    setFlag(1)
-    const headers = {'Content-Type': 'application/json'};
-    axios.post(BACKEND_URL + '/users/create_user_and_company',{
-      first_name:signupInput.first_name,
-      last_name:signupInput.last_name,
-      password:signupInput.password,
-      contact_no:signupInput.contact_no,
-      email_address:signupInput.email_address,
-      company_id:signupInput.company_id,
-      // users_type_id:signupInput.users_type_id
-    },{headers}).then((res) => {
-      console.log("Reponse of Sign up",res)
-      if(res.data.msg == "User already exits"){
-        toast("User Already Exists",{type:'error'})
-      }else{
-        toast("User Added SuccessFully",{type:'success'})
-        navigate('/seller/home')
-      }
-    }).catch((err) => {
-      console.log("Error in signup",err);
-      toast("Some Error in Sign Up",{type:'error'})
-    })
+    const requiredError = requiredFieldErrors();
+    if (Object.keys(requiredError).length > 0) {
+      setError(requiredError);
+      return;
+    }
+    setFlag(1);
+    const headers = { 'Content-Type': 'application/json' };
+    axios
+      .post(
+        BACKEND_URL + '/users/create_user_and_company',
+        {
+          first_name: signupInput.first_name,
+          last_name: signupInput.last_name,
+          password: signupInput.password,
+          contact_no: signupInput.contact_no,
+          email_address: signupInput.email_address,
+          company_id: signupInput.company_id,
+          // users_type_id:signupInput.users_type_id
+        },
+        { headers },
+      )
+      .then((res) => {
+        console.log('Reponse of Sign up', res);
+        if (res.data.msg == 'User already exits') {
+          toast('User Already Exists', { type: 'error' });
+        } else {
+          toast('User Added SuccessFully', { type: 'success' });
+          navigate('/seller/home');
+        }
+      })
+      .catch((err) => {
+        console.log('Error in signup', err);
+        toast('Some Error in Sign Up', { type: 'error' });
+      });
   };
 
   return (
@@ -83,6 +107,10 @@ const SignUpUser = () => {
               onChange={handleChangeInput}
             />
           </div>
+          <div className="flex justify-between">
+            {error && <p className="w-1/2 text-xs text-red-500">{error?.first_name}</p>}
+            {error && <p className="w-1/2 text-xs text-red-500">{error?.last_name}</p>}
+          </div>
           <Field
             type={'email'}
             id={'email_address'}
@@ -92,6 +120,8 @@ const SignUpUser = () => {
             value={signupInput['email_address']}
             onChange={handleChangeInput}
           />
+          {error && <p className="w-1/2 text-xs text-red-500">{error?.email_address}</p>}
+
           <Field
             type={'tel'}
             id={'contact_no'}
@@ -101,6 +131,7 @@ const SignUpUser = () => {
             value={signupInput['contact_no']}
             onChange={handleChangeInput}
           />
+          {error && <p className="w-1/2 text-xs text-red-500">{error?.contact_no}</p>}
           <Field
             type={'password'}
             id={'password'}
@@ -110,6 +141,8 @@ const SignUpUser = () => {
             value={signupInput['password']}
             onChange={handleChangeInput}
           />
+          {error && <p className="w-1/2 text-xs text-red-500">{error?.password}</p>}
+
           {/* <Field
             type={'number'}
             id={'users_type_id'}
@@ -127,15 +160,20 @@ const SignUpUser = () => {
           </div> */}
           <button
             type="button"
-            className=" mt-4 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mb-2 w-full rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300"
+            className=" dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mb-2 mt-4 w-full rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300"
             onClick={() => {
-              if(flag == 0){
-                handleSubmit()
+              if (flag == 0) {
+                handleSubmit();
               }
-            }}
-          >
-           Add User
+            }}>
+            Add User
           </button>
+          <div className="flex justify-center gap-1">
+            <p>Already have an account?</p>
+            <Link to={'/login'} className="text-decoration-none text-red-700">
+              Login
+            </Link>
+          </div>
           {/* <div className="text-center">
             <p className="text-sm">
               Already have an account?{' '}
