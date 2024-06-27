2d093a9b277c4bd87fbccff8a74bbdbd48599252
Jay-2003 <96811319+Jay-2003@users.noreply.github.com>
2024-06-27 11:09:30 +0530

    Only Truck Booking Is Show in sidebar.

--- src/common/components/sidebar/contants.js
+++ src/common/components/sidebar/contants.js
@@ -34,214 +34,240 @@ const mapModuleToLink = (module) => ({
   hoverIcon: module.icon,
 });
 
+sidebarLinks = [
+  {
+          title: 'Truck Booking',
+          // path: '/book',
+          icon: book,
+          hoverIcon: book,
+          subMenuOptions: [
+            !parseInt(is_admin) && {
+              title: 'Book',
+              path: '/book',
+            },
+            !parseInt(is_admin) && {
+              title: 'Create Indent',
+              path: '/indent',
+            },
+            !parseInt(is_admin) && {
+              title: 'All Indent',
+              path: '/all-indent/' + user_id,
+            },
+            parseInt(is_admin) && {
+              title: 'User Booking',
+              path: '/User',
+            },
+          ].filter((option) => option),
+        },
+]
 
-if(access_modules == null || access_modules?.length == 0){
-if (localStorage.getItem('is_super') != 3) {
-  sidebarLinks = [
-    {
-      title: 'Home',
-      path: '/seller/home',
-      icon: newHome,
-      hoverIcon: newHome,
-    },
-    parseInt(is_company)
-      ? {
-          title: 'Add User',
-          path: '/signup-user',
-          icon: user,
-          hoverIcon: user,
-        }
-      : null,
-    {
-      title: 'Dashboard',
-      path: '/dashboard',
-      icon: dashboard,
-      hoverIcon: dashboard,
-    },
-    {
-      title: 'Truck Booking',
-      // path: '/book',
-      icon: book,
-      hoverIcon: book,
-      subMenuOptions: [
-        !parseInt(is_admin) && {
-          title: 'Book',
-          path: '/book',
-        },
-        !parseInt(is_admin) && {
-          title: 'Create Indent',
-          path: '/indent',
-        },
-        !parseInt(is_admin) && {
-          title: 'All Indent',
-          path: '/all-indent/' + user_id,
-        },
-        parseInt(is_admin) && {
-          title: 'User Booking',
-          path: '/User',
-        },
-      ].filter((option) => option),
-    },
-    {
-      title: 'Orders',
-      path: '/orders',
-      icon: order,
-      hoverIcon: order,
-    },
-    {
-      title: 'Returns',
-      path: '/returns',
-      icon: return1,
-      hoverIcon: return1,
-    },
-    {
-      title: 'Delivery Boost',
-      path: '/',
-      icon: boost,
-      hoverIcon: boost,
-    },
-    {
-      title: 'Weight Management',
-      icon: weight,
-      hoverIcon: weight,
-      subMenuOptions: [
-        {
-          title: 'Weight Discrepancy',
-          path: '/billing-charge-details?page=1&perPage=15&to=&from=&status=0&search=&courier_id=',
-        },
-        {
-          title: 'Weight Freeze',
-          path: '/request-weight-freeze?freeze_status=Not%20Requested&serach=&per_page=&page=1&from=&to=&channel_code=',
-        },
-      ],
-    },
-    {
-      title: 'Setup & Manage',
-      icon: manage,
-      hoverIcon: manage,
-      subMenuOptions: [
-        {
-          title: 'Channels',
-          path: '/channels',
-        },
-        {
-          title: 'Catalogue',
-          path: '/catalogue',
-        },
-        {
-          title: 'Courier',
-          path: '/user-couriers',
-        },
-        {
-          title: 'Packaging',
-          path: '/',
-        },
-        {
-          title: 'Customers',
-          path: '/customers',
-        },
-      ],
-    },
-    // {
-    //   title: 'Buyer Experiance',
-    //   icon: user,
-    //   hoverIcon: user,
-    //   subMenuOptions: [
-    //     {
-    //       title: 'Brand Boost',
-    //       path: '/',
-    //     },
-    //     {
-    //       title: 'Brand Communication',
-    //       path: '/',
-    //     },
-    //     {
-    //       title: 'Return & Refunds',
-    //       path: '/',
-    //     },
-    //   ],
-    // },
-    {
-      title: 'Billing',
-      path: '/statement',
-      icon: bill,
-      hoverIcon: bill,
-    },
-    {
-      title: 'Tools',
-      icon: tool,
-      hoverIcon: tool,
-      subMenuOptions: [
-        {
-          title: 'Rate Calculator',
-          path: '/rate-calculator',
-        },
-        {
-          title: 'Rate Card',
-          path: '/',
-        },
-        {
-          title: 'Pincode Zone Mapping',
-          path: '/',
-        },
-        {
-          title: 'Bulk Print Actions',
-          path: '/',
-        },
-        {
-          title: 'Reports',
-          path: '/',
-        },
-        {
-          title: 'Activity Logs',
-          path: '/',
-        },
-      ],
-    },
-    {
-      title: 'Settings',
-      path: '/settings',
-      icon: settings,
-      hoverIcon: settings,
-    },
-    {
-      title: 'Help & Support',
-      path: '/',
-      icon: help,
-      hoverIcon: help,
-    },
-    {
-      title: 'Logout',
-      icon: home,
-      hoverIcon: homeActive,
-      onClick: logout,
-    },
-  ].filter((option) => option !== null);
-} else if (localStorage.getItem('is_super') == 3) {
-  sidebarLinks = [
-    {
-      title: 'KYC',
-      path: '/company-list',
-      icon: newHome,
-      hoverIcon: newHome,
-    },
-    {
-      title: 'COD Remittance',
-      path: '/remittance-logs',
-      icon: bill,
-      hoverIcon: bill,
-    },
-    {
-      title: 'Weight Discrepancy',
-      path: '/billing-charge-details?page=1&perPage=15&to=&from=&status=0&search=&courier_id=',
-      icon: weight,
-      hoverIcon: weight,
-    },
-  ];
-}
-}else{
-  sidebarLinks = modules
-  .filter((module) => access_modules?.includes(module.id))
-  .map((module) => mapModuleToLink(module))
-  .filter((link) => link !== null);
-}
+// if(access_modules == null || access_modules?.length == 0){
+// if (localStorage.getItem('is_super') != 3) {
+//   sidebarLinks = [
+//     {
+//       title: 'Home',
+//       path: '/seller/home',
+//       icon: newHome,
+//       hoverIcon: newHome,
+//     },
+//     parseInt(is_company)
+//       ? {
+//           title: 'Add User',
+//           path: '/signup-user',
+//           icon: user,
+//           hoverIcon: user,
+//         }
+//       : null,
+//     {
+//       title: 'Dashboard',
+//       path: '/dashboard',
+//       icon: dashboard,
+//       hoverIcon: dashboard,
+//     },
+//     {
+//       title: 'Truck Booking',
+//       // path: '/book',
+//       icon: book,
+//       hoverIcon: book,
+//       subMenuOptions: [
+//         !parseInt(is_admin) && {
+//           title: 'Book',
+//           path: '/book',
+//         },
+//         !parseInt(is_admin) && {
+//           title: 'Create Indent',
+//           path: '/indent',
+//         },
+//         !parseInt(is_admin) && {
+//           title: 'All Indent',
+//           path: '/all-indent/' + user_id,
+//         },
+//         parseInt(is_admin) && {
+//           title: 'User Booking',
+//           path: '/User',
+//         },
+//       ].filter((option) => option),
+//     },
+//     {
+//       title: 'Orders',
+//       path: '/orders',
+//       icon: order,
+//       hoverIcon: order,
+//     },
+//     {
+//       title: 'Returns',
+//       path: '/returns',
+//       icon: return1,
+//       hoverIcon: return1,
+//     },
+//     {
+//       title: 'Delivery Boost',
+//       path: '/',
+//       icon: boost,
+//       hoverIcon: boost,
+//     },
+//     {
+//       title: 'Weight Management',
+//       icon: weight,
+//       hoverIcon: weight,
+//       subMenuOptions: [
+//         {
+//           title: 'Weight Discrepancy',
+//           path: '/billing-charge-details?page=1&perPage=15&to=&from=&status=0&search=&courier_id=',
+//         },
+//         {
+//           title: 'Weight Freeze',
+//           path: '/request-weight-freeze?freeze_status=Not%20Requested&serach=&per_page=&page=1&from=&to=&channel_code=',
+//         },
+//       ],
+//     },
+//     {
+//       title: 'Setup & Manage',
+//       icon: manage,
+//       hoverIcon: manage,
+//       subMenuOptions: [
+//         {
+//           title: 'Channels',
+//           path: '/channels',
+//         },
+//         {
+//           title: 'Catalogue',
+//           path: '/catalogue',
+//         },
+//         {
+//           title: 'Courier',
+//           path: '/user-couriers',
+//         },
+//         {
+//           title: 'Packaging',
+//           path: '/',
+//         },
+//         {
+//           title: 'Customers',
+//           path: '/customers',
+//         },
+//       ],
+//     },
+//     // {
+//     //   title: 'Buyer Experiance',
+//     //   icon: user,
+//     //   hoverIcon: user,
+//     //   subMenuOptions: [
+//     //     {
+//     //       title: 'Brand Boost',
+//     //       path: '/',
+//     //     },
+//     //     {
+//     //       title: 'Brand Communication',
+//     //       path: '/',
+//     //     },
+//     //     {
+//     //       title: 'Return & Refunds',
+//     //       path: '/',
+//     //     },
+//     //   ],
+//     // },
+//     {
+//       title: 'Billing',
+//       path: '/statement',
+//       icon: bill,
+//       hoverIcon: bill,
+//     },
+//     {
+//       title: 'Tools',
+//       icon: tool,
+//       hoverIcon: tool,
+//       subMenuOptions: [
+//         {
+//           title: 'Rate Calculator',
+//           path: '/rate-calculator',
+//         },
+//         {
+//           title: 'Rate Card',
+//           path: '/',
+//         },
+//         {
+//           title: 'Pincode Zone Mapping',
+//           path: '/',
+//         },
+//         {
+//           title: 'Bulk Print Actions',
+//           path: '/',
+//         },
+//         {
+//           title: 'Reports',
+//           path: '/',
+//         },
+//         {
+//           title: 'Activity Logs',
+//           path: '/',
+//         },
+//       ],
+//     },
+//     {
+//       title: 'Settings',
+//       path: '/settings',
+//       icon: settings,
+//       hoverIcon: settings,
+//     },
+//     {
+//       title: 'Help & Support',
+//       path: '/',
+//       icon: help,
+//       hoverIcon: help,
+//     },
+//     {
+//       title: 'Logout',
+//       icon: home,
+//       hoverIcon: homeActive,
+//       onClick: logout,
+//     },
+//   ].filter((option) => option !== null);
+// } else if (localStorage.getItem('is_super') == 3) {
+//   sidebarLinks = [
+//     {
+//       title: 'KYC',
+//       path: '/company-list',
+//       icon: newHome,
+//       hoverIcon: newHome,
+//     },
+//     {
+//       title: 'COD Remittance',
+//       path: '/remittance-logs',
+//       icon: bill,
+//       hoverIcon: bill,
+//     },
+//     {
+//       title: 'Weight Discrepancy',
+//       path: '/billing-charge-details?page=1&perPage=15&to=&from=&status=0&search=&courier_id=',
+//       icon: weight,
+//       hoverIcon: weight,
+//     },
+//   ];
+// }
+// }else{
+//   sidebarLinks = modules
+//   .filter((module) => access_modules?.includes(module.id))
+//   .map((module) => mapModuleToLink(module))
+//   .filter((link) => link !== null);
+// }
